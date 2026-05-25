85ae05519ddd3e0b6632b41b230c01ca0bdd799c
3469987263hu <3469987263@qq.com>
2026-05-25 19:57:45 +0800

    Add Service Worker for offline support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'budget-v1';
+const FILES = [
+  '/budget/',
+  '/budget/index.html',
+  '/budget/manifest.json',
+  '/budget/icon-192.png',
+  '/budget/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && res.type === 'basic') {
+        const c = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, c));
+      }
+      return res;
+    }).catch(() => caches.match('/budget/index.html')))
+  );
+});
